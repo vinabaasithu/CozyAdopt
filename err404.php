8a2e5ad849114b5157094ea79270d33c945340fb
Ujang Jabbar <jabbarujang@gmail.com>
2018-05-08 15:08:37 +0700

    404 again

--- err404.php
+++ err404.php
@@ -14,22 +14,55 @@
     <div class="container">
       <div class="cat-gif-con">
         <img class="cat404" src="<?php echo $coz_domain ?>source/img/cat404.gif" alt="">
-        <!-- <?php
-          for ($i=1; $i <= 6; $i++) {
-            ?>
-            <div class="abso404 abso404<?php echo $i; ?>">
-             <img src="<?php echo $coz_domain ?>source/img/404.gif" alt="">
-            </div>
-            <?php
-          }
-         ?> -->
       </div>
-      <div class="cont-err-mess">
+      <div class="cont-err-desktop cont-err-mess">
         <div class="mess">
           <p class="h1 text-center">Error 404</p>
-          <p class="text-center lh"><strong>Upsss... Halaman yang kamu cari sudah dipindahkan, dihapus, atau tidak tersedia. Kamu dapat kembali kehalaman <a href="<?php echo $coz_domain ?>index.php">awal</a> jika kamu mau..</strong></p>
+          <p class="text-center lh" id="text-desktop"><strong>Upsss... Halaman yang kamu cari sudah dipindahkan, dihapus, atau tidak tersedia. Ayo.. kita kembali kehalaman awal <a class="h1" href="<?php echo $coz_domain ?>index.php">CozyAdopt</a></strong></p>
         </div>
       </div>
+      <div class='cont-err-mess-mobile cont-err-mess'><p></p></div>
     </div>
+    <script src="<?php echo $coz_domain; ?>source/js/jquery-3.3.1.min.js" charset="utf-8"></script>
+    <script type="text/javascript">
+      var p = $(".mess #text-desktop");
+      var cemp = $(".cont-err-desktop.cont-err-mess p.lh");
+      var cem2 = $(".cont-err-mess-mobile.cont-err-mess");
+      var cem2p = $(".cont-err-mess-mobile.cont-err-mess p");
+      var cgc = $(".cat-gif-con");
+      var a = p.html();
+      cem2p.html(a);
+      // function
+
+      // if window size
+      if ($(window).width() <= 901) {
+        $("body").hide();
+        setTimeout(function() {
+          cemp.hide();
+          cem2.show();
+          $("body").fadeIn(1600);
+          var cont_err_height = $(".cont-err-mess").height()*1.5;
+          cgc.css("top",cont_err_height);
+        }, 250);
+      } else {
+        setTimeout(function() {
+          $("body").fadeIn(1600);
+        }, 250);
+      }
+
+      // resize function
+      $(window).resize(function(){
+        if (window.matchMedia('(max-width: 901px)').matches) {
+          cemp.hide();
+          cem2.show();
+          var cont_err_height = $(".cont-err-mess").height()*1.5;
+          cgc.css("top",cont_err_height);
+        } else {
+          cgc.css("top", "0");
+          cem2.hide();
+          cemp.show();
+        }
+      });
+    </script>
   </body>
 </html>
